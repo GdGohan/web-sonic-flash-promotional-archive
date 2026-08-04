ae1cdc49d255e07c7e9c7218d07b2c0787dd2e99
Antonio Ross <70231641+GdGohan@users.noreply.github.com>
2026-08-04 12:13:02 -0300

    Add files via upload

---
+++ MSWinter/download_website.bat
@@ -0,0 +1,21 @@
+@echo off
+title Mirror do site - site
+
+REM Altere este caminho para onde deseja salvar os arquivos
+cd /d "%~dp0"
+
+wget ^
+  --mirror ^
+  --page-requisites ^
+  --convert-links ^
+  --adjust-extension ^
+  --restrict-file-names=windows ^
+  --trust-server-names ^
+  --no-parent ^
+  https://www.wondertrio.com/mike/websites/mswinter/images/
+
+echo.
+echo ==========================================
+echo Download concluido!
+echo ==========================================
+pause
